9a5746da1b13f6063694b74562f1d0f9e38d884d
fedinabli05 <fedi.nabli123@gmail.com>
2021-11-01 22:37:34 +0100

    Added implementation and interfaces in service layer

--- SolarCoffee.Web/Startup.cs
+++ SolarCoffee.Web/Startup.cs
@@ -1,19 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SolarCoffee.Data;
 using SolarCoffee.Services.Product;
+using SolarCoffee.Services.Product.Customer;
+using SolarCoffee.Services.Product.Inventory;
+using SolarCoffee.Services.Product.Order;
 
 namespace SolarCoffee.Web
 {
@@ -40,6 +36,9 @@ namespace SolarCoffee.Web
             });
 
             services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<ICustomerService, CustomerService>();
+            services.AddTransient<IInventoryService, InventoryService>();
+            services.AddTransient<IOrderService, OrderService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
